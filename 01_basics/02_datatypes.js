8e341effd19e68888db9a4eb0529ec3830f41233
Arti Patel <135823489+Arti192@users.noreply.github.com>
2024-08-05 13:42:57 +0000

    Conversion of data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict" // trear all js code as new version code
+
+// alert(3 + 3) We are using node js not browser.
+
+console.log(3 +
+     3)  // Code readibility should be high
+
+console.log("Arti")
+
+let name = "Arti"
+let age = 18
+let isLoggin = true
+let state= null;
+
+
+// number = 2 power 62
+// bigInt 
+// String = ""
+// boolean = true/false
+// undefined = value not assign
+// null = object
+// symbol = unique
+
+console.log(typeof null)
---
+++ 01_basics/03_datatypes_conversion.js
@@ -0,0 +1,29 @@
+let score = 33
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueNumber = Number(score)
+// console.log(typeof valueNumber)
+// console.log(valueNumber)
+
+// 33 => Number
+// "33abc" NAN
+// true => 1; false => 0;
+
+let isLoggined = "Alina"
+
+let booleanInLoggined = Boolean(isLoggined)
+// console.log(typeof booleanInLoggined)
+// console.log(booleanInLoggined)
+
+
+// 1 => true; 0 => false;
+// "" => false
+// "Alina" => true
+
+let someNumber = 33
+
+let stringNumber = String(33)
+console.log(typeof stringNumber)
+console.log(stringNumber)
