ead4f7ac551126bbaa354f4219dc1f71a17ff79a
Arti Patel <135823489+Arti192@users.noreply.github.com>
2024-08-05 14:14:08 +0000

    operation

--- 01_basics/03_datatypes_conversion.js
+++ 01_basics/03_datatypes_conversion.js
@@ -25,5 +25,31 @@ let booleanInLoggined = Boolean(isLoggined)
 let someNumber = 33
 
 let stringNumber = String(33)
-console.log(typeof stringNumber)
-console.log(stringNumber)
+// console.log(typeof stringNumber)
+// console.log(stringNumber)
+
+//************************ Operation ********************//
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2 + 2)
+// console.log(2 -2)
+// console.log(2 * 2)
+// console.log(2 / 2)
+// console.log(2 % 3)
+// console.log(2**3)
+
+let str1 = "Hello"
+let str2 = " Alina"
+// console.log(str1 + str2)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter)
